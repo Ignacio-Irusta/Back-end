429d3b79a2b28587c188cfcfaba8e76cfef268cd
Ignacio Irusta <ignacio_r99@hotmail.com>
2022-04-04 22:34:32 +0200

    Actividad clase 6

--- Clase6.js
+++ Clase6.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const express = require('express')
+
+const app = express();
 
 class Archivo {
     constructor() {
@@ -45,6 +48,11 @@ class Archivo {
         arrayJson.splice(0, arrayJson.length);    
         return console.log(arrayJson);
     }
+
+    async request(){
+        const datos = await fs.promises.readFile('productos.txt','utf-8')
+        return console.log(datos);
+    }
 }
 
 const articulo = new Archivo;
@@ -68,6 +76,45 @@ for (i in producto) {
     articulo.guardar(producto[i]);
 }
 
-console.log(articulo.getByIdAndAll());
+
+
+app.get('/productos', (req,res) => {
+    res.send(articulo);
+})
+app.get('/productosRandom', (req,res) => {
+        const u = Math.floor((Math.random() * (3-0))+0);
+
+    if(u == 0){
+        res.send(`
+        "title": "escuadra",
+		"price": 123,
+		"thumbnail": "http://www.google.com.ar",
+		"id": 1
+        `)
+    } else if (u == 1){
+        res.send(`
+        "title": "calculadora",
+		"price": 234,
+		"thumbnail": "http://www.google.com.ar",
+		"id": 2
+        `)
+    } else {
+        res.send(`
+        "title": "globo",
+		"price": 345,
+		"thumbnail": "http://www.google.com.ar",
+		"id": 3
+        `)
+    }
+})
+
+
+const PORT = 8080
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+
+/*console.log(articulo.getByIdAndAll());
 console.log(articulo.deleteById());
-console.log(articulo.deleteAll());
+console.log(articulo.deleteAll());*/
